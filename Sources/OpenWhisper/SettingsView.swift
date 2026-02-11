19e95ead73c863e6106462e28d06bbb8282970a9
Ivan Kuznetsov <kuznetsovivan496@gmail.com>
2026-02-11 17:52:20 +0200

    feat(hotkey): expose F13-F20 in common key picker

--- Sources/OpenWhisper/SettingsView.swift
+++ Sources/OpenWhisper/SettingsView.swift
@@ -736,7 +736,7 @@ struct SettingsView: View {
             ),
             (
                 title: "Function",
-                keys: ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12"]
+                keys: ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20"]
             ),
             (
                 title: "Punctuation",
